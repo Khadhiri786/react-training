4b49f8b978d60f9d389f962298184c683f97a9af
Khadhiri Abdul Subhan <ams.khadhiri@gmail.com>
2023-05-14 21:18:00 +0530

    Display table using list rendering

---
+++ src/Questions/Question1/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./index.css";
+
+type dataProps = {
+  empId: number;
+  employeeName: string;
+  employeeDesignation: string;
+  assignedProject: string;
+}[];
+const Question1: React.FC = () => {
+  const data: dataProps = [
+    {
+      empId: 2175,
+      employeeName: "Khadhiri",
+      employeeDesignation: "Associate",
+      assignedProject: "Deloitte",
+    },
+    {
+      empId: 2176,
+      employeeName: "Ram",
+      employeeDesignation: "Senior Associate",
+      assignedProject: "Health Care",
+    },
+    {
+      empId: 2177,
+      employeeName: "Jeyarani",
+      employeeDesignation: "Associate",
+      assignedProject: "FIELDA",
+    },
+    {
+      empId: 2178,
+      employeeName: "Ravi",
+      employeeDesignation: "Associate",
+      assignedProject: "Service Now",
+    },
+    {
+      empId: 2179,
+      employeeName: "Harsha",
+      employeeDesignation: "Lead Associate",
+      assignedProject: "HealthCare",
+    },
+  ];
+  return (
+    <React.Fragment>
+      <h1>Employee List</h1>
+      <table className="tableDetails">
+        <thead>
+          <tr>
+            <th>Employee Id</th>
+            <th>Employee Name</th>
+            <th>Employee Designation</th>
+            <th>Assigned Project</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((employeeDetail) => (
+            <tr>
+              <td>{employeeDetail?.empId}</td>
+              <td>{employeeDetail?.employeeName}</td>
+              <td>{employeeDetail?.employeeDesignation}</td>
+              <td>{employeeDetail?.assignedProject}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+};
+export default Question1;
